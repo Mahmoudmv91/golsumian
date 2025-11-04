3d44d13f14355f94b0b1fe9b3f078034fe49a9b6
mahmoud vakili <104832167+Mahmoudmv91@users.noreply.github.com>
2025-11-04 23:34:01 +0330

    first step

--- lib/sound_controller.dart
+++ lib/sound_controller.dart
@@ -1,15 +1,16 @@
+
 import 'package:audioplayers/audioplayers.dart';
 
 class SoundController{
 
 
-  final AudioPlayer _player = AudioPlayer();
+final AudioPlayer _player = AudioPlayer();
 
-  Future<void> playCorrect()async{
-    await _player.play(AssetSource('sounds/correct.mp3'));
-  }
+Future<void> playCorrect()async{
+await _player.play(AssetSource('sounds/correct.mp3'));
+}
 
-  Future<void> playWrong()async{
-    await _player.play(AssetSource('sounds/wrong.mp3'));
-  }
+Future<void> playWrong()async{
+await _player.play(AssetSource('sounds/wrong.mp3'));
+}
 }
